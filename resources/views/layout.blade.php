124a017b6c544a0d0ecf3e4db53f994024712203
david vanslambrouck <vanslambrouckd@users.noreply.github.com>
2015-03-26 18:26:55 +0100

    remove duplicate bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,16 @@
     <head>
     <meta charset="UTF-8">
     <title>Laravel blog test</title>
-        <link rel="stylesheet" href="{{ asset('/bootstrap/dist/css/bootstrap.css') }}">
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+
+        <!-- Fonts -->
+        <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <style type="text/css">
         body {
             padding-top:50px;
@@ -39,9 +48,9 @@
                 @yield('footer')
             </footer>
         </div>
-        <script src="{{ asset('/js/jquery.min.js') }}"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="{{ asset('/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+        
          <script type="text/javascript">
         /* <![CDATA[ */
         $(document).ready(function() {
